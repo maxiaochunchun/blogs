05a773ad0e76d3ff3237474365a2fad260e7e090
maxiaochun <378391590@qq.com>
2023-09-06 16:32:32 +0800

    11

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -13,8 +13,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
 
-git push -f https://github.com/maxiaochunchun/blogs.git master:gh-pages
+git push -f https://github.com/maxiaochunchun/blogs.git master:master
 
 cd -
